dd4f72721b6c233cc162cf2eba806e313576eda8
puttuo <douerding@yahoo.com>
2016-06-24 07:33:11 +0800

    Initial Commit

---
+++ c_hard_way/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c_hard_way
+//
+//  Created by admin in on 24/6/16.
+//  Copyright © 2016 admin in. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
