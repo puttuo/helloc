c17f7af4ea2efc6cbcd673f157b163a9c309bb45
puttuo <douerding@yahoo.com>
2016-07-05 21:49:56 +0800

    simplify the type definitions - define the type directly

--- c_hard_way/ex19.h
+++ c_hard_way/ex19.h
@@ -3,37 +3,33 @@
 
 #include "object.h"
 
-struct Monster{
+typedef struct Monster{
 	Object proto;
 	int hit_points;
-};
-
-typedef struct Monster Monster;
+} Monster;
 
 int Monster_attack(void *self, int damage);
 int Monster_init(void *self);
 
-struct Room {
+typedef struct Room {
 	Object proto;
 	Monster *bad_guy;
 	struct Room *north;
 	struct Room *south;
 	struct Room *east;
 	struct Room *west;
-};
+} Room;
 
-typedef struct Room Room;
 void *Room_move(void *self, Direction direction);
 int Room_attack(void *self, int damage);
 int Room_init(void *self);
 
-struct Map {
+typedef struct Map {
 	Object proto;
 	Room *start;
 	Room *location;
-};
+} Map;
 
-typedef struct Map Map;
 
 void *Map_move(void *self, Direction direction);
 int Map_attack(void *self, int damage);
--- c_hard_way/object.h
+++ c_hard_way/object.h
@@ -18,7 +18,7 @@
 	void Object_destroy(void *self);
 	void Object_describe(void *self);
 	void *Object_move(void *self, Direction direction);
-	int Object_attach(void *self, int damage);
+	int Object_attack(void *self, int damage);
 	void *Object_new(size_t size, Object proto, char *description);
 
 	#define NEW(T, N) Object_new(sizeof(T), T##Proto, N)
