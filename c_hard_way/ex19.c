480b478cf8ead991d2337f3f9d312398c4ad0ab1
Wang <midas.wang@emc.com>
2016-07-05 18:45:10 +0800

    update ex19.c

--- c_hard_way/ex19.c
+++ c_hard_way/ex19.c
@@ -85,5 +85,29 @@ int Map_attack(void *self, int damage) {
 }
 
 int Map_init(void *self) {
-	
+	Map *map = self;
+
+	//make some rooms for a small map
+	Room *hall = NEW(Room, "The great hall");
+	Room *throne = NEW(Room, "The throne room");
+	Room *arena = NEW(Room, "The arena, with the minotaur");
+	Room *kitchen = NEW(Room, "kitchen, you have the knife now");
+
+	//put the bad guy in the arena
+	arena->bad_guy = NEW(Monster, "The evil minotaur");
+
+	//setup the map rooms
+	hall->north = throne;
+	throne->west = arena;
+	throne->east = kitchen;
+	throne->south = hall;
+
+	arena->east = throne;
+	kitchen->west = throne;
+
+	//start the map and the character off in the hall
+	map->start = hall;
+	map->location = hall;
+
+	return 1;
 }
